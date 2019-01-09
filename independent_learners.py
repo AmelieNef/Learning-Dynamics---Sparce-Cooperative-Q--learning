8495e7c44e9946ce35f32aeeda2ce3373fd4d4ba
AmelieNef <45994072+AmelieNef@users.noreply.github.com>
2019-01-09 16:35:46 +0100

    Final version

--- independent_learners.py
+++ independent_learners.py
@@ -1,11 +1,13 @@
+import os
+import sys, getopt
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import warnings
+import multiprocessing as mp
 
 warnings.filterwarnings('ignore')
-np.random.seed(8455)  # Let numpy initialize the seed from the machine random entropy source
-st0 = np.random.get_state()  # Retrieve the initial state to be able to reproduce the results
 
 # state = array [x,y,z,t] with x,y,z,t in {0, ... ,9}
 # predator 1 location - prey location = [x,y]
@@ -29,8 +31,9 @@ def mapping(state, action):
     reward = -1
 
     # checking the state has all its values in {0, ... ,9}
-    if (state[0] < 0) or (state[0] > 9) or (state[1] < 0) or (state[1] > 9) or (state[2] < 0) or \
-            (state[2] > 9) or (state[3] < 0) or (state[3] > 9):
+    if (state[0] < 0) or (state[0] > 9) or (state[1] < 0) \
+            or (state[1] > 9) or (state[2] < 0) or (state[2] > 9) \
+            or (state[3] < 0) or (state[3] > 9):
         print("Relative positions out of bound")
 
     # moving the predators on the torus according to action
@@ -55,14 +58,11 @@ def mapping(state, action):
         captured = True
 
     # penalty for both predators jumping on the prey
-    if (manhattandistance(state[0], state[1]) == 1) \
-            and (manhattandistance(state[2], state[3]) == 1) \
-            and (manhattandistance(statefinal[0], statefinal[1]) == 0) \
-            and (manhattandistance(statefinal[2], statefinal[3]) == 0):
-        reward = -100
+    # penalty for both predators jumping on the same cell
+    if (statefinal[0] == statefinal[2]) and (statefinal[1] == statefinal[3]):
+        reward = -50
         for i in range(0, 4):
-            statefinal[i] = int(np.random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], size=1, \
-                                                 replace=True, p=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]))
+            statefinal[i] = int(np.random.choice(range(0, 10), size=1, replace=True))
 
     # penalty for one predators jumping on the prey while being alone
     if (manhattandistance(state[0], state[1]) == 1) \
@@ -90,72 +90,59 @@ def mapping(state, action):
 
     return (statefinal, reward, captured)
 
+def bestaction(Q, state):
+    """
+    Function to found the best action
+    :param dic:
+    :return: best action combined
+    """
+    M = Q[state[0]][state[1]][state[2]][state[3]][0]
+    act = 0
+    for k in range(0, 5):
+        if (Q[state[0]][state[1]][state[2]][state[3]][k] > M):
+             M = Q[state[0]][state[1]][state[2]][state[3]][k]
+             act = k
+    return (act, M)
 
-def q_learning(rseed):
+def q_learning(rseed, startingstates):
+    """
+    Function who running the algorithm of Q-learning
+    :param rseed:
+    :param startingstates: state from a random list
+    :return: list countain the number of step for each episode
+    """
     # Let numpy initialize the seed from the machine random entropy source
     np.random.seed(rseed)
-    # Retrieve the initial state to be able to reproduce the results
-    st0 = np.random.get_state()
-
-    # generating the 100 starting states
-    startingstates = np.zeros((100, 4), dtype=np.int)
-    for i in range(0, 100):
-        for j in range(0, 4):
-            startingstates[i][j] = int(np.random.choice(range(0, 10), size=1, replace=True))
 
     # params of the RL algo
-    N = 500000
+    N = 100000
     alpha = 0.3
     gamma = 0.9
 
     stepsarray = np.zeros(N, dtype=np.float)
-    captured = False
     start_time = time.time()
 
     # initialisation of the 2 matrices of Q values
     # Q = numpy array where multi index = ( state[0], ... , state[3], action predator 1 or 2 )
     Q1 = np.zeros((10, 10, 10, 10, 5), dtype=np.float)
     Q2 = np.zeros((10, 10, 10, 10, 5), dtype=np.float)
-    for k in range(0, 10):
-        for l in range(0, 10):
-            for m in range(0, 10):
-                for n in range(0, 10):
-                    for q in range(0, 5):
-                        Q1[k][l][m][n][q] = 37.5
-                        Q2[k][l][m][n][q] = 37.5
+    for agent1_x in range(0, 10):
+        for agent1_y in range(0, 10):
+            for agent2_x in range(0, 10):
+                for agent2_y in range(0, 10):
+                    for action in range(0, 5):
+                        Q1[agent1_x][agent1_y][agent2_x][agent2_y][action] = 75
+                        Q2[agent1_x][agent1_y][agent2_x][agent2_y][action] = 75
 
     # best action and Q value derived from Q in a given state
     # for each predator
-    def bestaction1(state):
-        M = Q1[state[0]][state[1]][state[2]][state[3]][0]
-        act = 0
-        for k in range(0, 5):
-            if (Q1[state[0]][state[1]][state[2]][state[3]][k] > M):
-                M = Q1[state[0]][state[1]][state[2]][state[3]][k]
-                act = k
-        return (act, M)
-
-    def bestaction2(state):
-        M = Q2[state[0]][state[1]][state[2]][state[3]][0]
-        act = 0
-        for k in range(0, 5):
-            if (Q2[state[0]][state[1]][state[2]][state[3]][k] > M):
-                M = Q2[state[0]][state[1]][state[2]][state[3]][k]
-                act = k
-        return (act, M)
 
     # Q learning algo
     for i in range(0, N):
-
-        # initial state and epsilon for the runs 0 to 99, 500 to 599, 1000 to 1099, ...
         if (i % 1000 < 500):
             state = startingstates[i % 100]
             epsilon = 0.2
-        # initial state and epsilon for the runs 100 to 499, 600 to 999, 1100 to 1499, ...
         else:
-            # for j in range(0,4):
-            #    state[j] = int ( np.random.choice( [0,1,2,3,4,5,6,7,8,9] , size=1 , \
-            #    replace=True , p=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1] ) )
             state = startingstates[i % 100]
             epsilon = 0
 
@@ -165,15 +152,15 @@ def q_learning(rseed):
         while (captured != True):
 
             # choose action for each predator according to epsilon greedy policy
-            if (np.random.random() < epsilon):
+            if (np.random.uniform() < epsilon):
                 a1 = int(np.random.choice([0, 1, 2, 3, 4], size=1, replace=False, p=[0.2, 0.2, 0.2, 0.2, 0.2]))
             else:
-                a1 = bestaction1(state)[0]
+                a1 = bestaction(Q1, state)[0]
 
             if (np.random.uniform() < epsilon):
                 a2 = int(np.random.choice([0, 1, 2, 3, 4], size=1, replace=False, p=[0.2, 0.2, 0.2, 0.2, 0.2]))
             else:
-                a2 = bestaction2(state)[0]
+                a2 = bestaction(Q2, state)[0]
 
             # observe result from action a
             a = (a1, a2)
@@ -182,11 +169,11 @@ def q_learning(rseed):
             # update Q1 and Q2
             Q1[state[0]][state[1]][state[2]][state[3]][a1] = \
                 (1 - alpha) * Q1[state[0]][state[1]][state[2]][state[3]][a1] + \
-                alpha * reward / 2 + alpha * gamma * bestaction1(state2)[1]
+                alpha * reward / 2 + alpha * gamma * bestaction(Q1,state2)[1]
 
             Q2[state[0]][state[1]][state[2]][state[3]][a2] = \
                 (1 - alpha) * Q2[state[0]][state[1]][state[2]][state[3]][a2] + \
-                alpha * reward / 2 + alpha * gamma * bestaction2(state2)[1]
+                alpha * reward / 2 + alpha * gamma * bestaction(Q2,state2)[1]
 
             state = state2
 
@@ -194,7 +181,7 @@ def q_learning(rseed):
 
         stepsarray[i] = steps
 
-        if (i % 10000 == 0):
+        if (i % 1000 == 0):
             print("Episode", i + 1, "finished in", steps, "steps.")
 
         i += 1
@@ -205,14 +192,23 @@ def q_learning(rseed):
 
 
 def main():
-    import multiprocessing as mp
-    seeds = [8642, 1489, 9952, 8995, 1962, 8483, 2021, 2161, 9628, 7462]
-    trials = 10
-    episodes = 500000
+    """
+    Function main
+    :return: /
+    """
+    np.random.seed(2161)
+    seeds = [np.random.randint(100, 10000) for _ in range(0, 100)]
+    # generating the 100 starting states
+    startingstates = np.zeros((100, 4), dtype=np.int)
+    for i in range(0, 100):
+        for j in range(0, 4):
+            startingstates[i][j] = int(np.random.choice(range(0, 10), size=1, replace=True))
+    trials = 1
+    episodes = 100000
     results = []
-    pool = mp.Pool(processes=2)
+    pool = mp.Pool(processes=1)
     for i in range(trials):
-        results.append(pool.apply_async(q_learning, (seeds[i],)))
+        results.append(pool.apply_async(q_learning, (seeds[i], startingstates,)))
     pool.close()
     pool.join()
 
@@ -225,6 +221,13 @@ def main():
     avg_steps = []
     for ts in range(episodes):
         avg_steps.append(np.average(episodes_step_arr[:, ts]))
+
+    times = int(episodes / 1000)
+    for i in range(0, times):
+        avg = np.average(avg_steps[i * 1000:i * 1000 + 1000])
+        print("avg from %d to %d : %f " % (i * 1000, i * 1000 + 1000, avg))
+
+    print("avg from %d to %d : %f " % (99500, 100000, np.average(avg_steps[99500:100000])))
 
     # charts
     style = ['b-', 'g-', 'r-', 'c-', 'm-', 'y-',
